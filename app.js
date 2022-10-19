8b1ad925e6c4a0c077c2ceeb6b3f791ddc8d0346
Donnovan Facchini <FacchiniDonnovan@MacBook-Pro-di-Donnovan.local>
2022-10-20 00:55:06 +0200

    Cambios en app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const APP_PORT = 3001;
+/* const APP_PORT = 3001; */
 
 const publicFolderPath = path.resolve('public');
 app.use(express.static(publicFolderPath));
@@ -11,7 +11,7 @@ app.use(express.static(publicFolderPath));
 ); */
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => console.log('Servidor corriendo en el puerto ${port}'));
+app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('views/home.html'))
