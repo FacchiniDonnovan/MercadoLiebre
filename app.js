75c1116313517e754d4128fed01e3814b1a1b713
Donnovan Facchini <FacchiniDonnovan@MacBook-Pro-di-Donnovan.local>
2022-10-19 20:19:03 +0200

    Cambios en package.json y en app.js.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const APP_PORT = 4001;
+const APP_PORT = 3001;
 
 const publicFolderPath = path.resolve('public');
 app.use(express.static(publicFolderPath));
 
-app.listen(APP_PORT, () =>
+/* app.listen(APP_PORT, () =>
     console.log('---------------> Servidor corriendo en puerto 4001.')
-);
+); */
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log('Servidor corriendo en el puerto ${port}'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('views/home.html'))
